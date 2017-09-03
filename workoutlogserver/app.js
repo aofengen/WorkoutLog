31f8f785721753ddf0fad8a1c8f28b493eabb89d
Aaron Ofengender <aofengen@gmail.com>
2017-09-03 14:47:46 -0400

    finished module 3. added app.js in server folder and set up simple req/res to test client and server sides

--- workoutlogserver/app.js
+++ workoutlogserver/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+// const http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(reg,res){
-	res.sendFile(__dirname + '/index.html');
+// app.get('/', function(reg,res){
+// 	res.sendFile(__dirname + '/index.html');
+// })
+
+app.use('/api/test', function(req,res){
+	res.send('Hello World');
 })
 
-http.listen(3000, function(){
-	console.log("Listening on port 3000")
+app.listen(3000, function(){
+	console.log("Listening on port 3000");
 })
